337da396741026d961ca1afd2a4183ba9af205e0
tu <php1007@163.com>
2021-01-05 19:17:25 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,17 @@ class UsersController extends Controller
     public function show(User $user)
     {
        return view('users.show',compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(UserRequest $request,User $user)
+    {
+        $user->update($request->all());
+        toast('个人资料更新成功！','success');
+        return redirect()->route('users.show',$user->id);
     }
 }
