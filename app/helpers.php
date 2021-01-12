90c4d5959d8f6cee7d141d34295f275ab48342a3
tu <php1007@163.com>
2021-01-12 18:13:59 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,12 @@
 function route_class() {
     return str_replace('.','-',Route::currentRouteName());
 }
+
+//导航栏active状态
+function active_class($category_id = 0) {
+    if($category_id) {
+        return request()->url() == route('categories.show',$category_id) ? "active" : '';
+    }
+        return request()->url() == route('topics.index') ? 'active' : '';
+
+}
